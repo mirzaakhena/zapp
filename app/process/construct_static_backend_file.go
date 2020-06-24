31ab061d5cb557c2509014c2d43bfd15cf57581f
mirza akhena <mirza.akhena@gmail.com>
2020-06-24 07:26:20 +0700

    update Api to RestAPI

--- app/process/construct_static_backend_file.go
+++ app/process/construct_static_backend_file.go
@@ -82,8 +82,20 @@ func createStaticBackendFile(tp *model.ThePackage) {
 		}
 
 		{
-			templateFile := fmt.Sprintf("../templates/backend/shared/config/config._go")
-			outputFile := fmt.Sprintf("../../../../%s/backend/shared/config/config.go", tp.PackagePath)
+			templateFile := fmt.Sprintf("../templates/backend/shared/config/contract._go")
+			outputFile := fmt.Sprintf("../../../../%s/backend/shared/config/contract.go", tp.PackagePath)
+			basic(tp, templateFile, outputFile, tp, 0664)
+		}
+
+		{
+			templateFile := fmt.Sprintf("../templates/backend/shared/config/implementation._go")
+			outputFile := fmt.Sprintf("../../../../%s/backend/shared/config/implementation.go", tp.PackagePath)
+			basic(tp, templateFile, outputFile, tp, 0664)
+		}
+
+		{
+			templateFile := fmt.Sprintf("../templates/backend/shared/config/public._go")
+			outputFile := fmt.Sprintf("../../../../%s/backend/shared/config/public.go", tp.PackagePath)
 			basic(tp, templateFile, outputFile, tp, 0664)
 		}
 
